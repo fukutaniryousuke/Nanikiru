5dd92b5dee35d0a0d27952d04eb8dc20d4aa0215
fukutaniryousuke <Fr83653554@gmail.com>
2022-06-10 08:46:24 +0000

    [add]chats_controller作成

---
+++ app/controllers/public/chats_controller.rb
@@ -0,0 +1,8 @@
+class Public::ChatsController < ApplicationController
+  
+  def show
+  end
+  
+  def create
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+#会員側
   devise_for :customers, skip:[:passwords], controllers: {
     registrations: "public/registrations",
     sessions: 'public/sessions'
@@ -8,25 +9,22 @@ Rails.application.routes.draw do
   devise_scope :customer do
     root to: "public/home#top"
     get '/about' => 'public/home#about'
-    #public/sessionsコントローラのguest_sign_inアクションを行う
-    post "customers/guest_sign_in", to: "public/sessions#guest_sign_in"
-
+    post "customers/guest_sign_in", to: "public/sessions#guest_sign_in" #public/sessionsコントローラのguest_sign_inアクションを行う
     resources :customers, only:[:show, :edit, :update], controller: "public/customers" do
-      resource :relationships, only:[:create, :destroy], controller: "public/relationships"
-      get '/followings' => 'public/relationships#followings'
-      get '/followers' => 'public/relationships#followers'
+      resource :relationships, only:[:create, :destroy], controller: "public/relationships"#フォロー機能
+      get '/followings' => 'public/relationships#followings'#フォロー一覧
+      get '/followers' => 'public/relationships#followers'#フォロワー一覧
     end
-    
-    get 'customers/:id/unsubscribe' => 'public/customers#unsubscribe', as: "unsubscribe"
-    patch '/customers/:id/withdrawal' => 'public/customers#withdrawal', as: "withdrawal"
-
+    get 'customers/:id/unsubscribe' => 'public/customers#unsubscribe', as: "unsubscribe" 
+    patch '/customers/:id/withdrawal' => 'public/customers#withdrawal', as: "withdrawal"#退会
     resources :post_images, only:[:new, :create, :show, :index, :edit, :update, :destroy], controller: "public/post_images" do
-       resource :favorites, only:[:create, :destroy], controller: "public/favorites"
-       resources :post_comments, only:[:create, :destroy], controller: "public/post_comments"
+       resource :favorites, only:[:create, :destroy], controller: "public/favorites"#いいね機能
+       resources :post_comments, only:[:create, :destroy], controller: "public/post_comments"#コメント機能
     end
-
+    resources :chats, only:[:show, :create]#DM機能
   end
-
+  
+#管理者側
   devise_for :admin, skip:[:registrations, :passwords], controllers: {
     sessions: "admin/sessions"
   }
