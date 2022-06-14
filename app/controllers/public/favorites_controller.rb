0269b1d81677a3e8795d3413aa1c86a4db290ae4
fukutaniryousuke <Fr83653554@gmail.com>
2022-06-14 06:53:18 +0000

    コメントの非同期化

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -1,22 +1,24 @@
 class Public::FavoritesController < ApplicationController
+  before_action :authenticate_customer!
+  before_action :set_post_image
   def create
     post_image = PostImage.find(params[:post_image_id])
     favorite = current_customer.favorites.new(post_image_id: post_image.id)
     favorite.save
-    redirect_back(fallback_location: root_path)
+    # redirect_back(fallback_location: root_path)
     post_image.create_notification_by(current_customer)
-    #必要ないかも？
-    # respond_to do |format|
-    #   format.html {redirect_to request.referrer}
-    #   format.js
-    #   end
+
   end
 
   def destroy
     post_image = PostImage.find(params[:post_image_id])
     favorite = current_customer.favorites.find_by(post_image_id: post_image.id)
     favorite.destroy
-    redirect_back(fallback_location: root_path)
+    # redirect_back(fallback_location: root_path)
+  end
+
+  def set_post_image
+     @post_image = PostImage.find(params[:post_image_id])
   end
 
 end
--- app/controllers/public/post_comments_controller.rb
+++ app/controllers/public/post_comments_controller.rb
@@ -1,21 +1,25 @@
 class Public::PostCommentsController < ApplicationController
   before_action :authenticate_customer!
+  before_action :set_post_image
   def create
     post_image = PostImage.find(params[:post_image_id])
     comment = current_customer.post_comments.new(post_comment_params)
     comment.post_image_id = post_image.id
     if comment.save
       comment.post_image.create_notification_comment!(current_customer, comment.id)
-      redirect_to post_image_path(post_image)
     end
   end
 
   def destroy
     PostComment.find(params[:id]).destroy
-    redirect_to post_image_path(params[:post_image_id])
+  end
+  
+  def set_post_image
+     @post_image = PostImage.find(params[:post_image_id])
   end
 
   private
+  
 
   def post_comment_params
     params.require(:post_comment).permit(:comment)
