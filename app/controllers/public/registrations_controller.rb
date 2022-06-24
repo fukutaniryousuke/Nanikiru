5c6bd8ed06e6e54c3fd336eadb66e0e9b8ce3f36
fukutaniryousuke <Fr83653554@gmail.com>
2022-06-24 03:16:14 +0000

    sign_up後遷移変更

--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -14,6 +14,10 @@ class Public::RegistrationsController < Devise::RegistrationsController
   #   super
   # end
 
+  def after_sign_up_path_for(resource)
+    customer_path(current_customer)
+  end
+
   # GET /resource/edit
   # def edit
   #   super
